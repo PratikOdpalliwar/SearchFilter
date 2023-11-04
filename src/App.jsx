143dcf62ac7bf30254991ba039d7be26527431dc
Pratik_Odpalliwar <pratikodpalliwar777@gmail.com>
2023-11-04 06:43:22 +0000

    logic and css is added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,39 @@
-import './App.css'
-import './MOCK_DATA.json'
+import './App.css';
+import data from './MOCK_DATA.js';
+import React, { useState } from 'react';
 
 function App() {
+  const [search, setSearch] = useState("");
+
   return (
-    <div>
-      Hello World
+<div className='App'>
+    
+    <div className="container">
+
+      <input type="text" placeholder="Search.." value={search} onChange={(e) => setSearch(e.target.value)} />
+
+      {data.filter((val) => {
+        if (search == "") {
+          return val;
+        }
+        else if (val.first_name.toLowerCase().includes(search.toLowerCase()
+        )) {
+          return val;
+        }
+      })
+        .map((val, index) => {
+          return (
+            <div className='arrayCont' key={index}>
+              <p>{val.first_name}</p>
+            </div>
+          );
+        }
+        )}
+
     </div>
-  )
+
+  </div>
+  );
 }
 
 export default App;
